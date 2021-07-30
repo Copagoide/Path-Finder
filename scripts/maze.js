bd832b72c5d04296bd93791cb04fea91fb90711c
Copagoide <84489895+Copagoide@users.noreply.github.com>
2021-07-30 14:11:14 -0400

    Add files via upload

--- scripts/maze.js
+++ scripts/maze.js
@@ -1,6 +1,12 @@
+function waitThisTime(ms) {
+    return new Promise (resolve => {
+        setTimeout(() => { resolve('') }, ms);
+    })
+}
+
 const mazeButton = document.querySelector("#mazeButton");
 
-mazeButton.addEventListener('click', () => {
+mazeButton.addEventListener('click', async () => {
 
     let stackY = [1];
     let stackX = [1];
@@ -12,13 +18,15 @@ mazeButton.addEventListener('click', () => {
         for (let j = 0; j <= 40; j++) {
             let cell = document.getElementById(i + "," + j);
             
-            if (cell.classList.contains("startNode") || cell.classList.contains("endNode")) { /* Do nothing */ }
-            else { $(cell).addClass("wall"); }
+            if (cell.classList.contains("startNode") || cell.classList.contains("endNode")) { }
+            else {
+                cell.classList.add("wall");
+            }
         }
     }
 
     for (let k = 0; k < 22*40; k++) {
-        
+
         if (visitedElements < mazeWidth * mazeHeight) {
             
             let y = stackY[stackY.length - 1]   //setting Y to the last index of the Y stack.
@@ -41,9 +49,8 @@ mazeButton.addEventListener('click', () => {
             if (stackY[stackY.length - 1] > 2) {
                 neighbourUp = document.getElementById(y - 2 + "," + x);
                 middleCellUp = document.getElementById(y - 1 + "," + x);
-                if (neighbourUp.classList.contains("visitedMazePath")) { }
-                else {
-                    $(neighbourUp).addClass("neighbour");
+                if (!neighbourUp.classList.contains("visitedMazePath")) { 
+                    neighbourUp.classList.add("neighbour");
                     neighbours.push(0);
                 }
             }
@@ -51,9 +58,8 @@ mazeButton.addEventListener('click', () => {
             if (stackX[stackX.length - 1] < 38) {
                 neighbourRight = document.getElementById(y + "," + (x + 2));
                 middleCellRight = document.getElementById(y + "," + (x + 1));
-                if (neighbourRight.classList.contains("visitedMazePath")) { }
-                else {
-                    $(neighbourRight).addClass("neighbour");
+                if (!neighbourRight.classList.contains("visitedMazePath")) {
+                    neighbourRight.classList.add("neighbour");
                     neighbours.push(1);
                 }
             }
@@ -61,9 +67,8 @@ mazeButton.addEventListener('click', () => {
             if (stackY[stackY.length - 1] < 20) {
                 neighbourDown = document.getElementById(y + 2 + "," + x);
                 middleCellDown = document.getElementById(y + 1 + "," + x);
-                if (neighbourDown.classList.contains("visitedMazePath")) { }
-                else {
-                    $(neighbourDown).addClass("neighbour");
+                if (!neighbourDown.classList.contains("visitedMazePath")) {
+                    neighbourDown.classList.add("neighbour");
                     neighbours.push(2);
                 }
             }
@@ -71,48 +76,49 @@ mazeButton.addEventListener('click', () => {
             if (stackX[stackX.length - 1] > 2) {
                 neighbourLeft = document.getElementById(y + "," + (x - 2));
                 middleCellLeft = document.getElementById(y + "," + (x - 1));
-                if (neighbourLeft.classList.contains("visitedMazePath")) { }
-                else {
-                    $(neighbourLeft).addClass("neighbour");
+                if (!neighbourLeft.classList.contains("visitedMazePath")) {
+                    neighbourLeft.classList.add("neighbour");
                     neighbours.push(3);
                 }
             }
 
             if (neighbours.length != 0) {
+                await waitThisTime(4);
+
                 let changeDirection = neighbours[Math.floor(Math.random() * neighbours.length)];      //randomly chooses a neighbour
                 switch(changeDirection) {
                     case 0:     //up
-                        $(neighbourUp).removeClass("neighbour");
-                        $(neighbourUp).addClass("visitedMazePath");
-                        $(middleCellUp).removeClass("wall")
-                        $(neighbourUp).removeClass("wall");
+                        neighbourUp.classList.remove("neighbour");
+                        neighbourUp.classList.add("visitedMazePath");
+                        middleCellUp.classList.remove("wall")
+                        neighbourUp.classList.remove("wall");
                         visitedElements++;
                         stackY.push((y - 2));
                         stackX.push(x);
                         break;
                     case 1:     //right
-                        $(neighbourRight).removeClass("neighbour");
-                        $(neighbourRight).addClass("visitedMazePath");
-                        $(middleCellRight).removeClass("wall")
-                        $(neighbourRight).removeClass("wall");
+                        neighbourRight.classList.remove("neighbour");
+                        neighbourRight.classList.add("visitedMazePath");
+                        middleCellRight.classList.remove("wall")
+                        neighbourRight.classList.remove("wall");
                         visitedElements++;
                         stackY.push(y);
                         stackX.push((x + 2));
                         break;
                     case 2:     //down
-                        $(neighbourDown).removeClass("neighbour");
-                        $(neighbourDown).addClass("visitedMazePath");
-                        $(middleCellDown).removeClass("wall")
-                        $(neighbourDown).removeClass("wall");
+                        neighbourDown.classList.remove("neighbour");
+                        neighbourDown.classList.add("visitedMazePath");
+                        middleCellDown.classList.remove("wall")
+                        neighbourDown.classList.remove("wall");
                         visitedElements++;
                         stackY.push((y + 2));
                         stackX.push(x);
                         break;
                     case 3:     //left
-                        $(neighbourLeft).removeClass("neighbour");
-                        $(neighbourLeft).addClass("visitedMazePath");
-                        $(middleCellLeft).removeClass("wall")
-                        $(neighbourLeft).removeClass("wall");
+                        neighbourLeft.classList.remove("neighbour");
+                        neighbourLeft.classList.add("visitedMazePath");
+                        middleCellLeft.classList.remove("wall")
+                        neighbourLeft.classList.remove("wall");
                         visitedElements++;
                         stackY.push(y);
                         stackX.push((x - 2));
@@ -130,8 +136,8 @@ mazeButton.addEventListener('click', () => {
         for (let j = 0; j <= 40; j++) {
             let cell = document.getElementById(i + "," + j);
             
-            $(cell).removeClass("visitedMazePath");
-            $(cell).removeClass("neighbour");  
+            cell.classList.remove("visitedMazePath");
+            cell.classList.remove("neighbour");  
         }
     }
     
